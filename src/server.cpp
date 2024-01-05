6913edb42911f180c529ef00e8304c6de3bbb4cc
Th0masAndy <yangxinpeng@zju.edu.cn>
2024-01-05 15:04:36 +0800

    ready for test

--- src/server.cpp
+++ src/server.cpp
@@ -2,21 +2,17 @@
 #include <iostream>
 #include <string>
 #include <zmq.hpp>
+#include "params.h"
 
 int main()
 {
-    EncryptionParameters parms(scheme_type::ckks);
-    long logN = 14;
-    size_t poly_modulus_degree = 1 << logN;
-    double scale = pow(2.0, 40);
-    parms.set_poly_modulus_degree(poly_modulus_degree);
-    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 58, 40, 40, 40, 40, 40, 40, 40, 40, 58 }));
+    auto parms = SealParameters::GetParameters1();
 
     Channel *c = new Channel(Channel::Type::Server);
 
     Server server(parms, c);
     server.recvParams();
-    server.run(Server::func::GELU);
+    server.run(Server::func::SOFTMAX);
 
     std::cout << (server.ckks->comm_recv) / 1024.0 / 1024.0 << " MB" << std::endl;
     std::cout << (server.ckks->comm_send) / 1024.0 / 1024.0 << " MB" << std::endl;
